595e82d186dd9d28ef70ce587bfaf31958a1fff7
Hayakuchi0 <hayakuchi@hinesm.info>
2018-11-13 22:09:52 +0900

    dclampと分離

--- dclamp/buildtools/buildDocker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-./downDocker.sh
-mv ../../install.php myweb
-mv ../../imageregist myweb
-docker-compose up -d --build
-mv myweb/install.php ../../
-mv myweb/imageregist ../../
-./myweb/debugDocker.sh
-docker exec -it myserver_apache2 /bin/bash /tmp/sqlinit.sh
--- dclamp/buildtools/myweb/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd tmp
-yum install -y httpd httpd-devel mysql php php-pdo php-mysql php-gd git
-php install.php
-systemctl enable httpd.service
