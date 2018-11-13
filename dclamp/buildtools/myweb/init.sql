afdb63d0b9716a0ac1df42ace40eafb06ade0e81
Hayakuchi0 <hayakuchi@hinesm.info>
2018-11-14 00:10:25 +0900

    dclampをgitignoreに追加

--- dclamp/buildtools/myweb/init.sql
+++
@@ -1,6 +0,0 @@
-create database imageregist default character set utf8 collate utf8_general_ci;
-use imageregist;
-create table users(
-username varchar(40) not null primary key,
-hashlist varchar(20000) not null
-);
